f63ae17dafef1ff67de13e89f29307f480870b17
piskova <v.piskova@beetroot.se>
2026-03-12 18:22:07 +0200

    add visualizer page base UI

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,7 +1,111 @@
+import { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { generate3DView } from "~/lib/ai.actions";
+import { Box, X, Download, Share2, RefreshCcw } from "lucide-react";
+import { Button } from "../components/ui/Button";
+
 const Visualizer = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const state = location.state as VisualizerLocationState;
+  const { initialImage, name, initialRender } = state;
+
+  const hasInitialGenerated = useRef<boolean>(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [currentImage, setCurrentImage] = useState<string | null>(
+    initialRender || null,
+  );
+
+  const handleGoBack = () => navigate("/");
+  const runGeneration = async () => {
+    if (!initialImage) return;
+    try {
+      setIsProcessing(true);
+      const result = await generate3DView({ sourceImage: initialImage });
+      if (result.renderedImage) {
+        setCurrentImage(result.renderedImage);
+        //TODO update rendered image in DB
+      }
+    } catch (error) {
+      console.error("3D generation failed", error);
+    } finally {
+      setIsProcessing(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!initialImage || hasInitialGenerated.current) return;
+    if (initialRender) {
+      setCurrentImage(initialRender);
+      hasInitialGenerated.current = true;
+    }
+
+    hasInitialGenerated.current = true;
+    runGeneration();
+  }, [initialImage, initialRender]);
+
   return (
-    <div>
-      Visualizer page
+    <div className="visualizer">
+      <nav className="topbar">
+        <div className="brand">
+          <Box className="logo" />
+
+          <span className="name">Roomify</span>
+        </div>
+        <Button
+          variant="ghost"
+          size="sm"
+          onClick={handleGoBack}
+          className="exit">
+          <X className="icon" /> Exit Editor
+        </Button>
+      </nav>
+      <section className="content">
+        <div className="panel">
+          <div className="panel-header">
+            <div className="panel-meta">
+              <p>Project</p>
+              <h2>{"TODO project name"}</h2>
+              <p className="note">Created by</p>
+            </div>
+            <div className="panel-actions">
+              <Button size="sm" className="export" disabled={!currentImage}>
+                <Download className="w-4 h-4 mr-2" /> Export
+              </Button>
+              <Button size="sm" onClick={() => {}} className="share">
+                <Share2 className="w-4 h-4 mr-2" />
+                Share
+              </Button>
+            </div>
+          </div>
+          <div
+            className={`render-area' ${isProcessing ? "is-processing" : ""}`}>
+            {currentImage ? (
+              <img src={currentImage} alt="AI render" />
+            ) : (
+              <div className="render-placeholder">
+                {initialImage && (
+                  <img
+                    src={initialImage}
+                    alt="Original"
+                    className="render-fallback"
+                  />
+                )}
+              </div>
+            )}
+            {isProcessing && (
+              <div className="render-overlay">
+                <div className="rendering-card">
+                  <RefreshCcw className="spinner"/>
+                  <span className="title">Rendering...</span>
+                  <span className="subtitle">Generating your 3D visualization</span>
+                </div>
+              </div>
+            )}
+          </div>
+
+        </div>
+      </section>
     </div>
   );
 };
